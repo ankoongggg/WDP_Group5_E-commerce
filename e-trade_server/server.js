d8bfb1e6e923b9a572b7208bb72c14a084ca4c66
Tus <dinhtubadao@gmail.com>
2026-01-10 12:55:58 +0700

    created server + client

---
+++ e-trade_server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const apiRoutes = require('./routes/allRoute'); 
+connectDB();
+
+app.use(express.json());
+
+app.get('/', async(req, res)=>{
+    try {
+        res.send({message: 'Welcome to Practical Exam!'});
+    } catch (error) {
+        res.send({error: error.message});
+    }
+});
+
+app.use('/api', apiRoutes);
+
+const PORT = process.env.PORT || 9999;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
