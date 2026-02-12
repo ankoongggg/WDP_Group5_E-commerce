1bfd9e05d946bdf7500123f0f6cd637d5bf24cc2
LDT <thangle84264@gmail.com>
2026-02-12 20:30:00 +0700

    modify: update model review product

--- e-trade_server/src/models/ReviewProduct.js
+++ e-trade_server/src/models/ReviewProduct.js
@@ -6,7 +6,7 @@ const reviewProductSchema = new mongoose.Schema({
     order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
     rating: { type: Number, required: true, min: 1, max: 5 },
     fileUploads: [String],
-    comment: { type: String, required: true },
+    comment: { type: String},
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
 });
