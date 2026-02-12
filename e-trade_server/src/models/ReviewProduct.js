85da24981752f429445b44981ab0eecd7f7faf52
LDT <thangle84264@gmail.com>
2026-02-12 20:18:20 +0700

    add: model review product

---
+++ e-trade_server/src/models/ReviewProduct.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const reviewProductSchema = new mongoose.Schema({
+    product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    fileUploads: [String],
+    comment: { type: String, required: true },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('ReviewProduct', reviewProductSchema);
